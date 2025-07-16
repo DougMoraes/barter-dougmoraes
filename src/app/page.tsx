8d2cd70d89976e032b20fa76ba6a997a25041a93
DougMoraes <dougmoraes91@gmail.com>
2025-07-16 22:15:18 +0200

    feat: add Header Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen">
-      <div className="w-full lg:w-2/3 px-6 md:px-12 py-10 flex flex-col">
+      <div className="w-full flex flex-col">
+        <Header />
       </div>
       <div className="hidden lg:flex w-1/3 bg-[var(--color-dark-background)] px-6 md:px-12 py-10 flex-col text-white">
       </div>
---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Image from 'next/image';
+
+export default function Header() {
+  return (
+    <header className="w-full md:px-12 py-12 sticky top-0 z-50 bg-transparent flex items-center gap-4">
+      <div className="bg-[var(--color-dark-text)] rounded-md w-10 h-10 flex items-center justify-center">
+        <Image
+          src="/letter.png"
+          alt="Barter icon"
+          height={24}
+          width={24}
+          className="h-6 w-6 object-contain"
+        />
+      </div>
+      <Image
+        src="/Barter.png" // "Barter" text as image
+        alt="Barter text logo"
+        height={24}
+        width={100}
+        className="h-6 w-auto object-contain"
+      />
+    </header>
+  );
+}
