c08f06e26dc61b8d81259c7a3dfdfb5671dcfe8b
DougMoraes <dougmoraes91@gmail.com>
2025-07-18 21:13:33 +0200

    feat: adjust partners icon sizes

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -4,56 +4,54 @@ import Image from 'next/image';
 
 export default function Partners() {
   return (
-    <footer className="w-full border-t border-[#D9E1DC] bg-transparent px-4 md:px-10 sticky bottom-0">
-      <div className="mx-auto py-6 flex justify-center items-center gap-6">
-        <p className="text-[14px] leading-none tracking-[2px] uppercase font-[400] text-center">
-          Trusted by
-        </p>
+    <footer className="w-full border-t border-[#D9E1DC] bg-transparent md:px-10 sticky bottom-0 flex justify-center items-center gap-6">
+      <p className="text-sm tracking-[2px] uppercase font-[300] text-center">
+        Trusted by
+      </p>
 
-        <div className="flex flex-wrap gap-x-4">
-          <Image
-            src="/partners/logo_anne_and_max.png"
-            alt="Anne and Max logo"
-            height={24}
-            width={100}
-            className="h-24 w-auto object-contain"
-          />
-          <Image
-            src="/partners/logo_moco.png"
-            alt="Moco logo"
-            height={24}
-            width={100}
-            className="h-24 w-auto object-contain"
-          />
-          <Image
-            src="/partners/logo_flink.png"
-            alt="Flink logo"
-            height={24}
-            width={100}
-            className="h-24 w-auto object-contain"
-          />
-          <Image
-            src="/partners/logo_louene.png"
-            alt="Louène logo"
-            height={24}
-            width={100}
-            className="h-24 w-auto object-contain"
-          />
-          <Image
-            src="/partners/logo_miniso.png"
-            alt="Miniso logo"
-            height={24}
-            width={100}
-            className="h-24 w-auto object-contain"
-          />
-          <Image
-            src="/partners/logo_statiegeld.png"
-            alt="Statiegeld Nederland logo"
-            height={24}
-            width={100}
-            className="h-24 w-auto object-contain"
-          />
-        </div>
+      <div className="flex flex-wrap gap-x-4">
+        <Image
+          src="/partners/logo_anne_and_max.png"
+          alt="Anne and Max logo"
+          height={24}
+          width={100}
+          className="h-18 w-auto object-contain"
+        />
+        <Image
+          src="/partners/logo_moco.png"
+          alt="Moco logo"
+          height={24}
+          width={100}
+          className="h-18 w-auto object-contain"
+        />
+        <Image
+          src="/partners/logo_flink.png"
+          alt="Flink logo"
+          height={24}
+          width={100}
+          className="h-18 w-auto object-contain"
+        />
+        <Image
+          src="/partners/logo_louene.png"
+          alt="Louène logo"
+          height={24}
+          width={100}
+          className="h-18 w-auto object-contain"
+        />
+        <Image
+          src="/partners/logo_miniso.png"
+          alt="Miniso logo"
+          height={24}
+          width={100}
+          className="h-18 w-auto object-contain"
+        />
+        <Image
+          src="/partners/logo_statiegeld.png"
+          alt="Statiegeld Nederland logo"
+          height={24}
+          width={100}
+          className="h-18 w-auto object-contain"
+        />
       </div>
     </footer>
   );
