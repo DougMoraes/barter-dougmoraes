f8b4f7d7d648ada2f75557c427fd406a4e3fd057
DougMoraes <dougmoraes91@gmail.com>
2025-07-18 20:58:03 +0200

    feat: set up initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import Footer from "@/components/Footer";
+import Partners from "@/components/Partners";
 import Header from "@/components/Header";
+import Section from "@/components/Section";
+import SignUp from "@/components/SignUp";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen">
-      <div className="w-full flex flex-col">
+      <div className="flex flex-col flex-grow">
         <Header />
-        <Footer />
+        <SignUp />
+        <Partners />
       </div>
-      <div className="hidden lg:flex w-1/3 bg-[var(--color-dark-background)] px-6 md:px-12 py-10 flex-col text-white">
-      </div>
+      <Section/>
     </main>
   );
 }
